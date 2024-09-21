7efcddd9c5431306a1b9bb46c1694e15194284c2
Jordan Zucker <jordan.zucker@gmail.com>
2024-09-21 15:38:53 -0700

    Trying to update things just slightly

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ const spawn = require('await-spawn');
 const mqtt = require("mqtt");
 const Constants = require('./constants');
 const HassSwitch = require("./hassSwitch");
+const { setPlaystationWake, setPlaystationStandby, getPlaystationInfo } = require("./playstation");
 const metricsMiddleware = promBundle({includeMethod: true});
 // actual framework is broken as a module :(
 // const playactor = require('playactor');
@@ -33,77 +34,19 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-const STANDBY_STATUS = 'STANDBY'; // eslint-disable-line no-unused-vars
-
-function PlayactorException(spawnException) {
-    this.spawnException = spawnException;
-    this.toString = function() {
-        return `${this.spawnException.code} => ${this.spawnException.stderr}`;
-    };
-    this.code = this.spawnException.code;
-}
-
-function ResponseParseException(value) {
-    this.value = value;
-    this.toString = function() {
-        return `${this.value}`;
-    };
-}
-
-const parseOutput = (commandLineOutput) => {
-    return JSON.parse(commandLineOutput);
-}
-
-const formatDeviceStatusResponse = (commandLineOutput) => {
-    try {
-        const parsedJSON = parseOutput(commandLineOutput);
-        const { type, status, name, id } = parsedJSON;
-        return {
-            'device': type,
-            'name': name,
-            'status': status,
-            'id': id
-        };
-    } catch (e) {
-        throw new ResponseParseException(e.toString());
-    }
-
-}
-
-// https://www.npmjs.com/package/await-spawn
-const executePlayactorScript = async (playactor_args) => {
-    // https://www.npmjs.com/package/await-spawn
-    try {
-        // playactor browse --timeout 10000
-        const result = await spawn('playactor', playactor_args);
-        return result.stdout.toString();
-    } catch (e) {
-        // console.error(`stdout: ${e.stdout.toString()}`);
-        // console.error(`stderr: ${e.stderr.toString()}`);
-        console.error(`got error code: ${e.code}`);
-        if (e.code === 1) {
-            return e.stdout.toString();
-        }
-        throw new PlayactorException(e);
-    }
-}
-
 // get
 app.get('/playactor/ps5/:ps5_ip', async(req, res) => {
     // cool tutorial
     // https://zellwk.com/blog/async-await-express/
     const { ps5_ip } = req.params;
-    console.log(`starting with ip: ${ps5_ip}`);
+    console.log(`server info starting with ip: ${ps5_ip}`);
 
-    const playactor_args = ['check', '--ip', ps5_ip, '--timeout', '5000'];
-    console.log(`playactor_args: ${playactor_args}`);
     try {
-        const results = await executePlayactorScript(playactor_args);
-        console.log(`got results ===> ${results}`);
-        const currentStatus = formatDeviceStatusResponse(results)
-        return res.json(currentStatus);
+        const results = await getPlaystationInfo(ps5_ip);
+        console.log(`server info got results ===> ${results}`);
+        return res.json(results);
     } catch (e) {
-        console.error(`returning error --> ${e.toString()}`);
+        console.error(`server info returning error --> ${e.toString()}`);
         return res.status(404).json({
             'message': e.toString()
         });
@@ -114,18 +57,16 @@ app.get('/playactor/ps5/:ps5_ip/wake', async(req, res) => {
     // cool tutorial
     // https://zellwk.com/blog/async-await-express/
     const { ps5_ip } = req.params;
-    console.log(`starting with ip: ${ps5_ip}`);
+    console.log(`server wake starting with ip: ${ps5_ip}`);
 
-    const playactor_args = ['wake', '--ip', ps5_ip, '--timeout', '5000', '--no-auth', '--connect-timeout', '5000'];
-    console.log(`playactor_args: ${playactor_args}`);
     try {
-        const results = await executePlayactorScript(playactor_args);
-        console.log(`got results ===> ${results}`);
+        const results = await setPlaystationWake(ps5_ip);
+        console.log(`server wake got results ===> ${results}`);
         return res.json({
             'message': 'ps5 awakened'
         });
     } catch (e) {
-        console.error(`returning error --> ${e.toString()}`);
+        console.error(`server wake returning error --> ${e.toString()}`);
         return res.status(404).json({
             'message': e.toString()
         });
@@ -136,18 +77,16 @@ app.get('/playactor/ps5/:ps5_ip/standby', async(req, res) => {
     // cool tutorial
     // https://zellwk.com/blog/async-await-express/
     const { ps5_ip } = req.params;
-    console.log(`starting with ip: ${ps5_ip}`);
+    console.log(`server standby starting with ip: ${ps5_ip}`);
 
-    const playactor_args = ['standby', '--ip', ps5_ip, '--timeout', '5000'];
-    console.log(`playactor_args: ${playactor_args}`);
     try {
-        const results = await executePlayactorScript(playactor_args);
-        console.log(`got results ===> ${results}`);
+        const results = await setPlaystationStandby(ps5_ip);
+        console.log(`server standby got results ===> ${results}`);
         return res.json({
             'message': 'ps5 asleep'
         });
     } catch (e) {
-        console.error(`returning error --> ${e.toString()}`);
+        console.error(`server standby returning error --> ${e.toString()}`);
         return res.status(404).json({
             'message': e.toString()
         });
