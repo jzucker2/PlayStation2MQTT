38acc57c4c97515afd7991d7d8655a06a51a444f
Jordan Zucker <jordan.zucker@gmail.com>
2024-09-23 13:49:54 -0700

    Another pass on some improvements

--- src/hassSensors.js
+++ src/hassSensors.js
@@ -7,7 +7,7 @@ const { getOrCreateServerID, getOrCreatePlayStationID } = require("./serverStore
 const serverID = getOrCreateServerID();
 const playstationID = getOrCreatePlayStationID();
 
-const getDevicePayload = (identifier, deviceName, model, manufacturer, viaDevice= undefined) => {
+const getDevicePayload = (identifier, deviceName, model, version, manufacturer, viaDevice= undefined) => {
     const finalPayload = {
         "identifiers": [
             identifier,
@@ -15,6 +15,7 @@ const getDevicePayload = (identifier, deviceName, model, manufacturer, viaDevice
         "name": deviceName,
         "manufacturer": manufacturer,
         "model": model,
+        "sw_version": version,
     };
     if (viaDevice) {
         finalPayload["via_device"] = viaDevice;
@@ -23,11 +24,11 @@ const getDevicePayload = (identifier, deviceName, model, manufacturer, viaDevice
 }
 
 const getServerDevicePayload = () => {
-    return getDevicePayload(serverID, "PlayStation2MQTT Bridge", "Bridge", Constants.APP_NAME);
+    return getDevicePayload(serverID, "PlayStation2MQTT Bridge", "Bridge", Constants.VERSION, Constants.APP_NAME);
 }
 
 const getPlaystationDevicePayload = () => {
-    return getDevicePayload(playstationID, "PlayStation", "PlayStation", "Sony", serverID);
+    return getDevicePayload(playstationID, "PlayStation", "PlayStation", "5", "Sony", serverID);
 }
 
 class HassBase {
